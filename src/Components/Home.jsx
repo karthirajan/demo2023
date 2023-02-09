d7939058b45b57964b3d5eb6a64b3c8bc1d7fc03
suruthi1995 <suruthikavi26@gmail.com>
2023-02-09 22:14:04 +0530

    New project

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,6 @@
 import { Component } from "react";
 import Login from "./Login";
 
-<<<<<<< HEAD
-class Demo extends Component{
-    render(){
-        
-            return (
-            <>
-            <div className="app"> APP </div>)
-            </>
-    
-    } 
-=======
 class Demo extends Component {
   render() {
     return (
@@ -23,7 +12,6 @@ class Demo extends Component {
       </div>
     );
   }
->>>>>>> 2d1d5cb5b139828d466a3aedb103271d47b4d7ba
 }
 
 export default Demo;
